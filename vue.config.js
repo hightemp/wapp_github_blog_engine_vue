261a076b0e900ec1b1976cbfb1720d5d9e543666
Панов Антон <hightemp.unknown@gmail.com>
2023-01-31 17:14:50 +0300

    update

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: "wapp_github_blog_engine_vue",
+  publicPath: "./",
   outputDir: "docs",
   transpileDependencies: true
 })
